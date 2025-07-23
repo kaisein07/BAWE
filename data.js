50904e5d291e5076744b96a8314dd541d56ebe6a
kaisein07 <babioabdoul93@gmail.com>
2025-07-23 17:45:59 +0100

    site de residance bawe

--- data.js
+++ data.js
@@ -129,7 +129,7 @@ const servicesData = [
 const galleryData = [
     {
         id: 1,
-        url: "https://drive.google.com/file/d/114Z9oOyIyzVzschULE4hLM3GtO1IEsND/view?usp=drive_link",
+        url: "https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
         caption: "Chambre Standard - Vue intérieure",
         category: "room",
         active: true
